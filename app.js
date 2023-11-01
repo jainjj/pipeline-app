53be76869be0a1055a7ae8330fd9290fa9331ab0
jainjj <102798920+jainjj@users.noreply.github.com>
2023-11-01 16:40:04 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+// Get a reference to the image element and the image container div.
+const imageElement = document.getElementById('display-image');
+const imageContainer = document.getElementById('image-container');
+
+// Replace the following URL with the actual image URL you want to display.
+const imageUrl = 'hack-week.png';
+
+// Set the image source to the specified URL.
+imageElement.src = imageUrl;
+
+// You can also customize the image properties, e.g., width and height.
+// imageElement.width = 300;
+// imageElement.height = 200;
+
+// Optional: Add some CSS styling to the image container.
+imageContainer.style.textAlign = 'center';
+
+// Optional: You can add more styling and functionality as needed.
